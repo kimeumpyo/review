fbfd983c65557902513e4fb6e77630b04493434b
kimeumpyo <eumpyo0409@gmail.com>
2023-07-19 15:05:48 +0900

    review

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+
+    default:
+      return state;
+  }
+}
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 }); // state의 초기값 => count : 0
 
   return (
     <div>
-      <h1>{count}</h1>
-      <button type="button" onClick={() => setCount(count - 1)}>
+      {/* <h1>{count}</h1> */}
+      <h1>{state.count}</h1>
+      {/* <button type="button" onClick={() => setCount(count - 1)}>
+        -1
+      </button> */}
+      {/* <button type="button" onClick={() => setCount(count + 1)}>
+        +1
+      </button> */}
+      <button type="button" onClick={() => dispatch({ type: "INCREMENT" })}>
         -1
       </button>
-      <button type="button" onClick={() => setCount(count + 1)}>
+      <button type="button" onClick={() => dispatch({ type: "DECREMENT" })}>
         +1
       </button>
     </div>
